c45157d07be4e6fdd2423394e2e02bc0db59fa88
matthewjlewis93 <matthewjlewis93@gmail.com>
2024-04-09 15:35:08 -0400

    added to showWeatherDetails function

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -6,8 +6,20 @@ const city = document.getElementById('city').value;
 const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}&units=metric`;
 
 // const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}`;
-
+document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
 function showWeatherDetails(event) {
     event.preventDefault();
-
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const weatherInfo = document.getElementById('weatherInfo');
+      weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                              <p>Temperature: ${data.main.temp} &#8451;</p>
+                              <p>Weather: ${data.weather[0].description}</p>`;
+    })
+    .catch(error => {
+        console.error('Error fetching weather:', error);
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+    });
 }
